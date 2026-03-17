00517ced88c8e9d9115158eb602b8099fb7a7246
Emre Eker <EmreEker@outlook.dk>
2026-03-17 17:44:42 +0100

    add arguments for exec

--- README.md
+++ README.md
@@ -4,15 +4,21 @@
 * Java 21 
 * Maven
 
-### Install the game 
+## Install the game 
 ```bash
 mvn clean install
 ```
 
-### Run the game
+## Run the game
 ```bash
 mvn exec:java
 ```
+### Arguments for the game
+The two agents in the game and the AI thinking time can be given with these arguments
+```bash
+-Dagent1=player -Dagent2=ai -Dtime=1000
+```
+Agent1 is always starting and is black
 
 ## Run tests
 ```bash
--- src/main/java/org/dtu/introai/boardgame/Main.java
+++ src/main/java/org/dtu/introai/boardgame/Main.java
@@ -2,6 +2,7 @@ package org.dtu.introai.boardgame;
 
 import org.dtu.introai.boardgame.agents.MonteCarlo;
 import org.dtu.introai.boardgame.agents.Player;
+import org.dtu.introai.boardgame.api.Agent;
 import org.dtu.introai.boardgame.othello.GameFrame;
 import org.dtu.introai.boardgame.util.Cell;
 
@@ -9,7 +10,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        new GameFrame(new MonteCarlo(Cell.BLACK, 500), new MonteCarlo(Cell.WHITE, 2000)).start();
+        String agent1 = args[0];
+        String agent2 = args[1];
+        long time = Long.parseLong(args[2]);
+
+        Agent blackAgent;
+        if(agent1.equals("player")) {
+            blackAgent = new Player();
+        }else{
+            blackAgent = new MonteCarlo(Cell.BLACK,time);
+        }
+
+        Agent whiteAgent;
+        if(agent2.equals("ai")){
+            whiteAgent = new MonteCarlo(Cell.WHITE,time);
+        }else{
+            whiteAgent = new Player();
+        }
+
+        new GameFrame(blackAgent, whiteAgent).start();
 
     }
 }
