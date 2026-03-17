02e738f4fa43c634aec7026fe0184fcfee019137
Emre Eker <EmreEker@outlook.dk>
2026-03-17 10:10:39 +0100

    MCTreeMode impl

--- src/main/java/org/dtu/introai/boardgame/agents/types/MCTreeNode.java
+++ src/main/java/org/dtu/introai/boardgame/agents/types/MCTreeNode.java
@@ -1,6 +1,46 @@
 package org.dtu.introai.boardgame.agents.types;
 
+import java.util.ArrayList;
+
 public class MCTreeNode {
 
+    private int wins;
+    private int losses;
+    private int visits;
+    private ArrayList<MCTreeNode> children;
 
+    public MCTreeNode(){
+        this.children = new ArrayList<>();
+    }
+
+    public ArrayList<MCTreeNode> getChildren() {
+        return children;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getVisits() {
+        return visits;
+    }
+
+    public void incrementVisit(){
+        visits++;
+    }
+
+    public void incrementWins(){
+        wins++;
+    }
+    public void incrementLosses(){
+        losses++;
+    }
+
+    public void addChild(MCTreeNode child){
+        children.add(child);
+    }
 }
